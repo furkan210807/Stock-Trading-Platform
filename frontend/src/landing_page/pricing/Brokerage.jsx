7c34f2069fbc59d2d28510ca105f17f2ea14ae4d
furkan210807 <furkan210807@gmail.com>
2026-07-10 18:36:37 +0530

    Brokerage

--- frontend/src/landing_page/pricing/Brokerage.jsx
+++ frontend/src/landing_page/pricing/Brokerage.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 function Brokerage() {
     return ( 
-        <h1>Brokerage</h1>
+          <div className="container">
+          
+            <div className="row p-5 mt-3 text-center border-top text-muted">
+               <div className="col-8 p-4">
+               <a href=""><h3 className='fs-5'>Brokerage Calculator</h3></a>
+               </div>
+
+               <div className="col-4 p-4">
+                <a href=""><h3 className='fs-5'>List of Charges</h3></a>
+               </div>
+
+
+               
+
+               
+            </div>
+        </div>
      );
+    
 }
 
 export default Brokerage;
