15b24bf7e521f3bb2fcd8553e95ea41e758b23ed
furkan210807 <furkan210807@gmail.com>
2026-07-24 23:59:54 +0530

    add new

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Stock-Trading-Platform
 A modern stock trading platform inspired by Zerodha with features like market watch, stock buying/selling, portfolio tracking, and analytics dashboard.
+
+This is Build Up by Md Furkan Alam
--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React from  "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
+import { useState } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
